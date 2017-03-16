ce3687335133cc3dbc8cc0d7d387d199b799a887
DeMaio, Dominick <dldemaio@metlife.com>
2017-03-16 14:56:30 -0600

    Initial Commit

---
+++ Alexa_GWV/Alexa_GWV.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Alexa_GWV.Web.Startup))]
+namespace Alexa_GWV.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
